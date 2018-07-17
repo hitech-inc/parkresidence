9631fcb1745538b91492d44e0721a2ad1c1fbee5
Arman85 <advanced315@gmail.com>
2018-07-17 11:48:23 +0600

    added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,3 @@
+<div style="margin: 0 auto; text-align: center; display: flex; justify-content: center; align-items: center; padding: 10% 0; font-size: 1.5rem;">
+	<h1 style="font-family: 'Bold'">Страница не найдена</h1>	
+</div>
